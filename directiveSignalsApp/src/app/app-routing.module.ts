fb18e69e3fad6c95d4a97c86aa701f66fe0ef09a
Gerts Adasme <gadasmef@gmail.com>
2024-06-22 13:20:20 -0400

    fix: add signals module

--- directiveSignalsApp/src/app/app-routing.module.ts
+++ directiveSignalsApp/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'products', loadChildren: () => import('./products/products.module').then(m => m.ProductsModule) },
+  { path: 'signals', loadChildren: () => import('./signals/signals.module').then(m => m.SignalsModule) },
   { path: 'shared', loadChildren: () => import('./shared/shared.module').then(m => m.SharedModule) },
   { path: '**', redirectTo: 'products' }
 ];
---
+++ directiveSignalsApp/src/app/signals/layout/signals-layout/signals-layout.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+import { RouterModule } from '@angular/router';
+
+@Component({
+    selector: 'app-signals-layout',
+    templateUrl: './signals-layout.component.html',
+    styleUrl: './signals-layout.component.css'
+})
+export class SignalsLayoutComponent { }
