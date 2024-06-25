b8ceea7a82dca35d9e1ed44b0c8f15042b247f18
Gerts Adasme <gadasmef@gmail.com>
2024-06-24 20:04:03 -0400

    feat: add side-menu component

--- directiveSignalsApp/src/app/signals/signals.module.ts
+++ directiveSignalsApp/src/app/signals/signals.module.ts
@@ -3,11 +3,13 @@ import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
+import { SideMenuComponent } from './components/side-menu/side-menu.component';
 
 
 @NgModule({
   declarations: [
-    SignalsLayoutComponent
+    SignalsLayoutComponent,
+    SideMenuComponent
   ],
   imports: [
     CommonModule,
