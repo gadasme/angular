7f7c8583324b6c10d25e1625fd9b2339991c05e3
Gerts Adasme <gadasmef@gmail.com>
2024-08-19 19:37:50 -0400

    feat: add user-info page component

--- directiveSignalsApp/src/app/app.module.ts
+++ directiveSignalsApp/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
@@ -10,12 +10,12 @@ import { ReactiveFormsModule } from '@angular/forms';
     AppComponent
   ],
   imports: [
-    BrowserModule,
     AppRoutingModule,
+    BrowserModule,
     ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(withFetch())
   ],
   bootstrap: [AppComponent]
 })
--- directiveSignalsApp/src/app/signals/pages/user-info-page/user-info-page.component.ts
+++ directiveSignalsApp/src/app/signals/pages/user-info-page/user-info-page.component.ts
@@ -1,7 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit, signal } from '@angular/core';
+import { UsersService } from '../../services/users.service';
+import { User } from '../../interfaces/user-response.interface';
 
 @Component({
     selector: 'app-user-info-page',
     templateUrl: './user-info-page.component.html',
 })
-export class UserInfoPageComponent { }
+export class UserInfoPageComponent implements OnInit {
+
+    private usersService = inject(UsersService);
+    public userId = signal(1);
+    public currentUser = signal<User | undefined>(undefined);
+    public userWasFound = signal(true);
+    
+    ngOnInit(): void {
+        this.loadUser(this.userId());
+    }
+
+    loadUser(id: number) {
+        if (id <= 0) return;
+        this.userId.set(id);
+        this.currentUser.set(undefined);
+        
+        this.usersService.getUserById(id)
+            .subscribe(user => {
+                this.currentUser.set(user);
+            })
+
+    }
+
+ }
