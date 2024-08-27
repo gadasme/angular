49ad9715a79b668b5fb4e3afb7b3a9fc205e85c6
Gerts Adasme <gadasmef@gmail.com>
2024-08-26 22:03:37 -0400

    feat: add properties-page component with signals and effects

--- directiveSignalsApp/src/app/signals/pages/properties-page/properties-page.component.ts
+++ directiveSignalsApp/src/app/signals/pages/properties-page/properties-page.component.ts
@@ -1,7 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, computed, effect, OnDestroy, OnInit, signal } from '@angular/core';
+import { User } from '../../interfaces/user-response.interface';
 
 @Component({
-    selector: 'app-properties-page',    
-    templateUrl: './properties-page.component.html',
+    selector: 'app-properties-page',
+    templateUrl: './properties-page.component.html'
 })
-export class PropertiesPageComponent { }
+export class PropertiesPageComponent implements OnDestroy, OnInit {    
+
+    public user = signal<User>({
+        id: 2,
+        email: 'janet.weaver@reqres.in',
+        first_name: 'Janet',
+        last_name: 'Weaver',
+        avatar: 'https://reqres.in/img/faces/2-image.jpg'
+    });
+
+    public counter = signal(10);
+
+    public fullName = computed(() => `${ this.user().first_name } ${ this.user().last_name }`);
+
+    public userChangedEffect = effect(() => {
+       console.log(`${this.user().first_name} - ${this.counter()}`);
+    });
+
+    ngOnDestroy(): void {        
+    }    
+
+    ngOnInit(): void {
+        setInterval(() => {
+            this.counter.update( current => current + 1);
+            if (this.counter() === 15) {
+                this.userChangedEffect.destroy();
+            }
+        }, 1000);
+    }    
+
+    increaseBy(value: number) {
+        this.counter.update(current => current + value);
+    }
+
+    onFieldUpdated(field: keyof User, value: string) {
+        this.user.update(current => {
+            switch(field) {
+            case 'email':
+                current.email = value;
+                break;
+            case 'avatar':
+                current.avatar = value;
+                break;
+            case 'first_name':
+                current.first_name = value;
+                break;
+            case 'last_name': 
+                current.last_name = value;
+                break;
+            case 'id':
+                current.id = Number(value);
+                break;
+            }
+            return current;
+        });
+    }
+
+}
--- directiveSignalsApp/src/app/signals/signals.module.ts
+++ directiveSignalsApp/src/app/signals/signals.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
 import { SignalsRoutingModule } from './signals-routing.module';
 import { SignalsLayoutComponent } from './layout/signals-layout/signals-layout.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.component';
+import { CommonModule } from '@angular/common';
+import { PropertiesPageComponent } from './pages/properties-page/properties-page.component';
 
 
 @NgModule({
@@ -12,10 +13,11 @@ import { UserInfoPageComponent } from './pages/user-info-page/user-info-page.com
     SignalsLayoutComponent,
     SideMenuComponent,
     UserInfoPageComponent,
+    PropertiesPageComponent,
   ],
   imports: [
+    SignalsRoutingModule,
     CommonModule,
-    SignalsRoutingModule
   ]
 })
 export class SignalsModule { }
