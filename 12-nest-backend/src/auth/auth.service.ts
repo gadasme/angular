c5817a9bca43de89b1424e8f3f28ec639def1cc4
Gerts Adasme <gadasmef@falabella.cl>
2024-09-18 12:19:41 -0300

    feat: add bcryptjs hashSync method use

--- 12-nest-backend/src/auth/auth.service.ts
+++ 12-nest-backend/src/auth/auth.service.ts
@@ -4,11 +4,13 @@ import {
   Injectable,
   InternalServerErrorException,
 } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import * as bcryptjs from 'bcryptjs';
+
 import { CrateUserDto } from './dto/create-user.dto';
 import { UpdateAuthDto } from './dto/update-auth.dto';
-import { InjectModel } from '@nestjs/mongoose';
 import { User } from './entities/user.entity';
-import { Model } from 'mongoose';
 
 @Injectable()
 export class AuthService {
@@ -19,13 +21,17 @@ export class AuthService {
 
   async create(createUserDto: CrateUserDto): Promise<User> {
     try {
-      const newUser = new this.userModel(createUserDto);
-
       // 1- Encriptar la contraseña
+      const { password, ...userData } = createUserDto;
+      const newUser = new this.userModel({
+        password: bcryptjs.hashSync(password, 10),
+        ...userData,
+      });
+      await newUser.save();
+      const { password: _, ...user } = newUser.toJSON();
+      return user;
       // 2- Guardar el usuario
       // 3- Generar el JWT
-
-      return await newUser.save();
     } catch (error) {
       console.log('error :>> ', error.code);
       if (error.code === '11000') {
