e9c231b93a36025a8df68d0bb58de6522712b8e8
Gerts Adasme <gadasmef@gmail.com>
2024-10-23 21:55:34 -0300

    feat: add interfaces for auth

--- 13-authApp/src/app/auth/services/auth.service.ts
+++ 13-authApp/src/app/auth/services/auth.service.ts
@@ -1,9 +1,25 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { Observable, of } from 'rxjs';
+import { AuthStatus, User } from '../interfaces';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
+  private readonly baseUrl: string = environment.baseUrl;
+  private http: HttpClient = inject( HttpClient );
+
+  private _currentUser = signal<User|null>(null);
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
   constructor() { }
+
+  login(email: string, password: string): Observable<boolean> {
+    return of(true);
+  }
+
+
 }
