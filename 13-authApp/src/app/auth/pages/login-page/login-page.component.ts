ff5b5f5672a0d0cccc9b8438b7ddd83855c08b21
Gerts Adasme <gadasmef@gmail.com>
2024-10-24 22:12:31 -0300

    feat: add sweetalert2

--- 13-authApp/src/app/auth/pages/login-page/login-page.component.ts
+++ 13-authApp/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,5 +1,8 @@
 import { Component, inject } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login-page',
@@ -9,14 +12,22 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class LoginPageComponent {
 
   private fb = inject( FormBuilder );
+  private authService = inject(AuthService);
 
   public myForm: FormGroup = this.fb.group({
-    email:    ['', [ Validators.required, Validators.email ]], 
-    password: ['', [ Validators.required, Validators.minLength(6) ]],
+    email:    ['a@a.com', [ Validators.required, Validators.email ]], 
+    password: ['123456', [ Validators.required, Validators.minLength(6) ]],
   });
 
   login() {
-    console.log(this.myForm.value);
+    const { email, password } = this.myForm.value;
+    this.authService.login(email, password)
+      .subscribe({
+        next: () => console.log('Todo bien!'),
+        error: (message) => {
+          Swal.fire('Error', message, 'error');
+        }
+      });
   }
 
 }
