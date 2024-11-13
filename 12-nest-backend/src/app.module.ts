5ca8037533db46ae31f37c481cd828740e58a31c
Gerts Adasme <gadasmef@gmail.com>
2024-11-13 20:02:04 -0300

    fix: update package.json

--- 12-nest-backend/src/app.module.ts
+++ 12-nest-backend/src/app.module.ts
@@ -3,11 +3,12 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthModule } from './auth/auth.module';
-
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(process.env.MONGO_URI, {
+      dbName: process.env.MONGO_DB_NAME,
+    }),
     AuthModule,
   ],
   controllers: [],
