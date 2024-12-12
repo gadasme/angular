7739d667edfab212fc9473e1573913354c8736a1
Gerts Adasme <gadasmef@gmail.com>
2024-12-11 23:02:33 -0300

    feat: add toogle action to the todo.reducer

--- redux/03-todoapp/src/app/todos/todo-item/todo-item.component.ts
+++ redux/03-todoapp/src/app/todos/todo-item/todo-item.component.ts
@@ -1,6 +1,9 @@
 import { Component, ElementRef, Input, OnInit, ViewChild } from '@angular/core';
 import { Todo } from '../models/todo.model';
 import { FormControl, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { AppState } from '../../app.reducer';
+import * as actions from '../todo.actions';
 
 @Component({
   selector: 'app-todo-item',
@@ -17,11 +20,16 @@ export class TodoItemComponent implements OnInit {
 
   editando: boolean = false;
 
-  constructor() {}
+  constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {      
     this.chkCompletado = new FormControl(this.todo?.completado);
     this.txtInput = new FormControl(this.todo?.texto, Validators.required);
+
+    this.chkCompletado.valueChanges.subscribe(valor => {
+      console.log({valor});
+      this.store.dispatch(actions.toggle({ id: this.todo.id }));
+    });
   }
 
   editar() {
